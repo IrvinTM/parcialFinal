95d01671fd33404b41b1896bcee2c7aa01c6d901
Irvin <irvint698@gmail.com>
2023-11-21 15:58:59 -0600

    1

--- src/logica/Principal.java
+++ src/logica/Principal.java
@@ -4,6 +4,8 @@
  */
 package logica;
 
+import interfaz.PantallaPrincipal;
+
 /**
  *
  * @author Test
@@ -17,9 +19,15 @@ public class Principal {
         //nuevo objeto paciente Paciente(String nombre, int edad, double peso, double altura,
      //String tipoMascota, String nombreDueno, String raza)
 
+     // call the principal from package interfaz
+
+         PantallaPrincipal principal = new PantallaPrincipal();
+            principal.setVisible(true);
+
         Paciente paciente1 = new Paciente("Firulais", 5, 10, 0.5, "Perro", "Juan", "Chihuahua");
        System.out.println(paciente1.toString());
        
+         
        
     }
     
